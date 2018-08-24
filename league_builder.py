91f57e087ffad98845ac58f75d2439385f6e7af5
Andrew Hosna <andrew@hosna.com>
2018-08-23 20:14:35 -0500

    added functions build_roster and create_welcome_letter

--- league_builder.py
+++ league_builder.py
@@ -1,15 +1,15 @@
-# project 1: build a soccer league
 import csv
 import random
 
 WELCOME_LETTER = """Dear {},
 {} has been placed on the {} for the upcoming season!
 
-We will have the first practice on {}.
+We will have the first practice on August 25th at 10:00 AM.
 
 Hope to see you all there!
 Coach
 """
+
 
 # randomly and evenly assign a list of players to each team in the league
 def assign_to_team(league, player_list):
@@ -20,8 +20,27 @@ def assign_to_team(league, player_list):
             player_list.remove(random_player)
 
 
+# export welcome letter for a player as 'firstName_lastName.txt'
+def create_welcome_letter(player, team):
+    welcome_filename = "_".join(player[0].lower().split()) + ".txt"
+    with open(welcome_filename, "w") as output_file:
+        output_file.write(WELCOME_LETTER.format(player[3], player[0], team))
+
+
+# export team names and rosters to 'teams.txt'
+# generate a welcome letter when a player is added to a team roster
+def build_roster(league):
+    with open("teams.txt", "w") as output_file:
+        for team_name, team_roster in league.items():
+            output_file.write("{}\n".format(team_name) + "=" * len(team_name))
+            for player in team_roster:
+                output_file.write("\n{0}, {1}, {2}, {3}".format(*player))
+                create_welcome_letter(player, team_name)
+            output_file.write("\n\n")
+
+
 if __name__ == '__main__':
-    league = {"Dragons": [], "Sharks": [], "Raptors": []}
+    soccer_league = {"Dragons": [], "Sharks": [], "Raptors": []}
     experienced_players = list()
     rookie_players = list()
 
@@ -38,21 +57,8 @@ if __name__ == '__main__':
                                        row['Guardian Name(s)']))
 
     # randomly assign an equal number of experienced and rookie players to each team
-    assign_to_team(league, experienced_players)
-    assign_to_team(league, rookie_players)
+    assign_to_team(soccer_league, experienced_players)
+    assign_to_team(soccer_league, rookie_players)
 
-    # export the league roster into 'teams.txt'
-    with open("teams.txt", "w") as output_file:
-        output_file.write("Soccer League")
-        for team_name, team_roster in league.items():
-            output_file.write("\n\n{}\n".format(team_name) + "=" * len(team_name))
-            for name, height, experience, guardian in team_roster:
-                output_file.write("\n{}, {}, {}, {}".format(name, height, experience, guardian))
-
-    # export welcome letters for each player as 'firstname_lastname.txt'
-    for team_name, team_roster in league.items():
-        first_practice = "August 25th at 10:00 AM"
-        for player in team_roster:
-            welcome_file = "_".join(player[0].lower().split()) + ".txt"
-            with open(welcome_file, "w") as output_file:
-                output_file.write(WELCOME_LETTER.format(player[3], player[0], team_name, first_practice))
+    # export league roster in 'teams.txt', create welcome letter for each player
+    build_roster(soccer_league)
